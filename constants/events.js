d4253592a7f353d0dd4846da4dd6894d6f7e65d2
Gabby-Tech1 <gkaquainoo@gmail.com>
2025-01-20 08:05:26 +0000

    Changed the images of the event section on the home page

--- constants/events.js
+++ constants/events.js
@@ -1,7 +1,9 @@
-import Event1 from '@/public/Event1.png'
+import Event1 from '@/public/events/event1.png'
+import Event2 from '@/public/events/event2.png'
+import Event3 from '@/public/events/event3.png'
 
 export const events = [
     {image: Event1, title: "Annual Worship Conference", description: "Join us for three days of powerful worship and ministry", venue: "123 Faith Avenue, Accra, Ghana", time: "12:30AM", date: {day: "15", month: "Febraury", year: "2024"}},
-    {image: Event1, title: "Community Outreach", description: "Serving our local community through acts of kindness", venue: "123 Faith Avenue, Accra, Ghana", time: "12:30AM", date: {day: "15", month: "Febraury", year: "2024"}},
-    {image: Event1, title: "Youth Revival", description: "A youth program fostering growth through education, and personal development", venue: "123 Faith Avenue, Accra, Ghana", time: "12:30AM", date: {day: "15", month: "Febraury", year: "2024"}},
+    {image: Event2, title: "Community Outreach", description: "Serving our local community through acts of kindness", venue: "123 Faith Avenue, Accra, Ghana", time: "12:30AM", date: {day: "15", month: "Febraury", year: "2024"}},
+    {image: Event3, title: "Youth Revival", description: "A youth program fostering growth through education, and personal development", venue: "123 Faith Avenue, Accra, Ghana", time: "12:30AM", date: {day: "15", month: "Febraury", year: "2024"}},
 ]
